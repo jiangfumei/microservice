f450ee02a7a5d9b24673940b4711b65933454617
fumei.jiang <fumei.jiang@code365.com>
2020-01-16 19:01:16 +0800

    add user 、 role

---
+++ sysadmin/src/main/java/com/cloud/sysadmin/entity/UserRole.java
@@ -0,0 +1,36 @@
+package com.cloud.sysadmin.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "user_role", schema = "microservice", catalog = "")
+public class UserRole {
+    private long id;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserRole userRole = (UserRole) o;
+        return id == userRole.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
