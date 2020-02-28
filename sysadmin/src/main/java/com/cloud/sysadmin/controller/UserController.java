ee7e6afca47181a6872283817896ff1224d401a9
fumei.jiang <fumei.jiang@code365.com>
2020-02-28 16:49:28 +0800

    add redis to common

--- sysadmin/src/main/java/com/cloud/sysadmin/controller/UserController.java
+++ sysadmin/src/main/java/com/cloud/sysadmin/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.cloud.sysadmin.controller;
 
+import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@Api("user")
 @RequestMapping(value = "/user")
 public class UserController {
 
