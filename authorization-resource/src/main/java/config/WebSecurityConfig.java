99c964ce1f8b9be5a7c50fce91111830e3349664
fumei.jiang <fumei.jiang@code365.com>
2020-03-02 15:22:33 +0800

    add websecurityconfig.java

---
+++ authorization-resource/src/main/java/config/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http//
+                .requestMatchers().antMatchers("/", "/resources/**","/login", "/registe","/api/**", //
+                "/v2/**", "/swagger-resources/**", "/swagger-ui.html", "/configuration/**")//
+                .and()//
+                .authorizeRequests().antMatchers("/resources/**").authenticated().anyRequest().permitAll()//
+                .and().csrf().disable().httpBasic();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+
+
+}
--- sysadmin/src/main/java/com/cloud/sysadmin/controller/UserController.java
+++ sysadmin/src/main/java/com/cloud/sysadmin/controller/UserController.java
@@ -3,11 +3,8 @@ package com.cloud.sysadmin.controller;
 import com.cloud.common.base.vo.Result;
 import com.cloud.common.util.ResultUtil;
 import com.cloud.sysadmin.entity.User;
-import io.netty.util.internal.StringUtil;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.Assert;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
