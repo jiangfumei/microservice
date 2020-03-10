1aa373ef90a34464ff0441de4821042b6c8f049e
fumei.jiang <fumei.jiang@code365.com>
2020-03-10 19:54:18 +0800

    MyFilterSecurityInterceptor error

--- authorization/src/main/java/com/cloud/authorization/admin/security/permission/MyFilterSecurityInterceptor.java
+++ authorization/src/main/java/com/cloud/authorization/admin/security/permission/MyFilterSecurityInterceptor.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.SecurityMetadataSource;
 import org.springframework.security.access.intercept.AbstractSecurityInterceptor;
+import org.springframework.security.access.intercept.InterceptorStatusToken;
+import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
 import org.springframework.stereotype.Component;
 
@@ -11,13 +13,16 @@ import javax.annotation.Resource;
 import javax.servlet.*;
 import java.io.IOException;
 
+/**
+ * 权限管理过滤器
+ * 监控用户行为
+ */
 
 @Component
 @Slf4j
 public class MyFilterSecurityInterceptor extends AbstractSecurityInterceptor implements Filter {
 
-    @Resource
-    private FilterInvocationSecurityMetadataSource securityMetadataSource;
+    private FilterInvocationSecurityMetadataSource securityMetadataSource;//两种基于spring security实现动态权限的方法，一是自定义accessDecisionManager，二是自定义FilterInvocationSecurityMetadataSource。
 
     @Autowired
     public void setMyAccessDecisionManager(MyAccessDecisionManager myAccessDecisionManager) {
@@ -32,9 +37,30 @@ public class MyFilterSecurityInterceptor extends AbstractSecurityInterceptor imp
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        FilterInvocation fi = new FilterInvocation(request, response, chain);
+        invoke(fi);
     }
+
+    public void invoke(FilterInvocation fi) throws IOException, ServletException {
+
+        InterceptorStatusToken token = super.beforeInvocation(fi);
+        try {
+            fi.getChain().doFilter(fi.getRequest(), fi.getResponse());
+        } finally {
+            super.afterInvocation(token, null);
+        }
+    }
+
+    /*public FilterInvocationSecurityMetadataSource getSecurityMetadataSource() {
+        return this.securityMetadataSource;
+    }
+
+    public void setSecurityMetadataSource(FilterInvocationSecurityMetadataSource newSource) {
+        this.securityMetadataSource = newSource;
+    }
+*/
+
 
     @Override
     public void destroy() {
@@ -43,11 +69,11 @@ public class MyFilterSecurityInterceptor extends AbstractSecurityInterceptor imp
 
     @Override
     public Class<?> getSecureObjectClass() {
-        return null;
+        return FilterInvocation.class;
     }
 
     @Override
     public SecurityMetadataSource obtainSecurityMetadataSource() {
-        return null;
+        return this.securityMetadataSource;
     }
 }
