e6bfaded22bcef643d062b082f4d0633ae882121
fumei.jiang <fumei.jiang@code365.com>
2020-04-13 11:40:52 +0800

    adjust pom

--- sysadmin/src/test/java/com/cloud/sysadmin/SysadminApplicationTests.java
+++ sysadmin/src/test/java/com/cloud/sysadmin/SysadminApplicationTests.java
@@ -4,14 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
-@SpringBootTest
+/*@SpringBootTest
 public class SysadminApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
-    @Test
+   *//* @Test
     public void pass() {
         String pwd = "123456";
         String encodePwd = BCrypt.hashpw(pwd, BCrypt.gensalt()); // 加密，核心代码
@@ -19,12 +19,6 @@ public class SysadminApplicationTests {
         boolean flag = BCrypt.checkpw(pwd, encodePwd); // 验证加密是否正确
         System.out.println(flag);
     }
-    /*public static void main(String[] args) {
-        String pwd = "123456";
-        String encodePwd = BCrypt.hashpw(pwd, BCrypt.gensalt()); // 加密，核心代码
-        System.out.println(encodePwd);
-        boolean flag = BCrypt.checkpw(pwd, encodePwd); // 验证加密是否正确
-        System.out.println(flag);
-    }*/
 
-}
+*//*
+}*/
